612c7ad9cfe0dc4d427ffef74dbf05f059a969de
Sean Spencer <seang.spencer@mail.utoronto.ca>
2025-05-23 15:11:11 -0400

    added setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'TariffPilot',
+    version= '0.0.0',
+    author= 'Patel Shehbaz, Sean Spencer',
+    author_email= 'patelshehbaz27@gmail.com, sgspencer2618@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)   
